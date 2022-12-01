63a61f3c1c1139f7e3fa92f71700ac83def11027
Ansarissab <zahidensari116@gmail.com>
2022-12-01 18:56:32 +0500

    Reduced number of js files

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+var script = document.createElement('script');
+  script.type = 'text/javascript';
+
+  script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js';
+  document.body.appendChild(script);
+
+var script = document.createElement('script');
+  script.type = 'text/javascript';
+
+  script.src = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js';
+  document.body.appendChild(script);
+
+var script = document.createElement('script');
+  script.type = 'text/javascript';
+
+  script.src = 'https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js';
+  document.body.appendChild(script);
+
+
+var script = document.createElement('script');
+  script.type = 'text/javascript';
+
+  script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js';
+  document.body.appendChild(script);
