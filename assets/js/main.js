06b717ac15af7dba6f0356906f2dd7c4aff0766c
Ansarissab <zahidensari116@gmail.com>
2022-12-01 19:06:05 +0500

    Reduced number of css files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,30 @@
-var script = document.createElement('script');
-  script.type = 'text/javascript';
 
-  script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js';
-  document.body.appendChild(script);
+var loadStylesheet = function(url, callback) {
+  var head = document.getElementsByTagName('head')[0];
+  var cssnode = document.createElement('link');
+  cssnode.type = 'text/css';
+  cssnode.rel = 'stylesheet';
+  cssnode.href = url;
+  cssnode.onreadystatechange = callback;
+  cssnode.onload = callback;
+  head.appendChild(cssnode);
+}
 
-var script = document.createElement('script');
-  script.type = 'text/javascript';
+var loadJavascriptFile = function(url){
+  var script = document.createElement('script');
+    script.type = 'text/javascript';
 
-  script.src = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js';
-  document.body.appendChild(script);
+    script.src = url;
+    document.body.appendChild(script);
+}
 
-var script = document.createElement('script');
-  script.type = 'text/javascript';
+loadStylesheet("https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css");
+loadStylesheet("https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css");
+loadStylesheet("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap");
+loadStylesheet("https://pro.fontawesome.com/releases/v5.10.0/css/all.css");
 
-  script.src = 'https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js';
-  document.body.appendChild(script);
 
-
-var script = document.createElement('script');
-  script.type = 'text/javascript';
-
-  script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js';
-  document.body.appendChild(script);
+loadJavascriptFile('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js');
+loadJavascriptFile('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js');
+loadJavascriptFile('https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js');
+loadJavascriptFile('https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js');
